727ad50dc1a7fe4d8603189e012beaa224f48084
sanjunaikl <sanjusamson446@gmail.com>
2023-01-06 17:37:52 +0530

    first commit

---
+++ dbutil.java
@@ -0,0 +1,10 @@
+class demo
+{
+	public void main(string args[])
+	{
+		for (a=1;a<+5;a++)
+		{
+			system.out.println(a)
+		}
+	}
+}
---
+++ string.java
@@ -0,0 +1,11 @@
+class demo
+{
+        public void main(string args[])
+        {
+                for (a=1;a<+5;a++)
+                {
+                        system.out.println(a)
+                }
+        }
+}
+
